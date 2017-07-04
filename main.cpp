014f9851385a1f39c7d8d386163b7253b153c045
linkineo <linkineo@linkineo.com>
2017-07-04 22:54:23 +0200

    🚀:project setup

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <CImg.h>
+#include <json.hpp>
+#include <boost/filesystem.hpp>
+
+#include <iostream>
+
+
+int main(int argc, char** agrv)
+{
+
+using json = nlohmann::json;
+
+
+return 0;
+
+}
